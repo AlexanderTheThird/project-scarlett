cf74289f411cd1b01da65db190e343c96268bb1d
The One <35406008+ManWithNoWorry@users.noreply.github.com>
2019-08-05 16:21:28 +0300

    Примечание!
    Распакуйте 'plugins.7z'. :)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$('.owl-carousel').owlCarousel({
+	    loop: true,
+	    autoplay: true,
+	    autoplayTimeout: 5000,
+	    nav: true,
+	    dots: true,
+	    items: 1
+	})
+
+
+	// $(window).on("load",function(){
+	//     $(".content").mCustomScrollbar({
+	//     	autoHideScrollbar: true
+	//     });
+	// });
+
+});	
+
+
